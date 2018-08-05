1a808e986b98b72698c387dd113267741b017b4c
Matthew Brunelle <ciferkey@gmail.com>
2018-08-05 18:16:12 -0400

    Updated handler to use a mono internally.

--- src/main/kotlin/com/matthewbrunelle/blog/UrlHandler.kt
+++ src/main/kotlin/com/matthewbrunelle/blog/UrlHandler.kt
@@ -6,27 +6,31 @@ import reactor.core.publisher.Mono
 import java.net.URLDecoder
 import org.jsoup.Jsoup
 import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.BodyInserters.fromPublisher
+import org.springframework.web.reactive.function.client.WebClient
 import java.nio.charset.StandardCharsets
 
 @Suppress("UNUSED_PARAMETER")
 class UrlHandler(private val urlDecoder: URLDecoder) {
     val charSet = StandardCharsets.UTF_8.toString()
 
+    val client = WebClient.create("")
+
     fun unfurl(request: ServerRequest): Mono<ServerResponse> {
         // TODO: handling null url
+
         val url = URLDecoder.decode(request.pathVariable("url"), charSet)
 
-        val document = Jsoup.connect(url).get()
-
-        // TODO: url matching -> correct extractor
-        // instagram, youtube
-
-        val result = openGradeExtract(document)
-                ?: defaultExtract(document)
+        val response = client.get().uri(url).retrieve().bodyToMono(String::class.java)
+                .map {
+                    val document = Jsoup.parse(it)
+                    openGradeExtract(document)
+                            ?: defaultExtract(document)
+                }
 
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .syncBody(result)
+                .body(fromPublisher(response, UnfurlResponse::class.java))
     }
 }
 
